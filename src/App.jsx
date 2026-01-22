a9018c1b20ef9f513091521330ed5a76ceacd184
Angello Arenas <angelloarenas2013@gmail.com>
2026-01-22 15:50:18 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </div>
+  );
+}
+export default App;
